4973510ba4b26e0a673f03ba59ec352a785412fc
Diego Castilho <diego.castilho@me.com>
2026-02-27 18:15:26 -0300

    Initial Commit

---
+++ nightscout-modern-app/nightscout_modern_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  nightscout_modern_appApp.swift
+//  nightscout-modern-app
+//
+//  Created by Diego Castilho on 27/02/26.
+//
+
+import SwiftUI
+
+@main
+struct nightscout_modern_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
